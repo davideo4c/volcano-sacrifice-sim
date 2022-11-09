3bd042f2686d3b773381416b4da5ec749a8e0e79
David Forsee <forsee.david@gmail.com>
2022-11-09 13:39:46 -0500

    Update DDOL.cs

--- Assets/DDOL.cs
+++ Assets/DDOL.cs
@@ -7,7 +7,7 @@ public class DDOL : MonoBehaviour
     // Start is called before the first frame update
     private void Awake()
     {
-        DontDestroyOnLoad(this);
-        Debug.Log("Don't destroy on load: " + this.name);
+        DontDestroyOnLoad(gameObject);
+        Debug.Log("Don't destroy on load: " + gameObject.name);
     }
 }
